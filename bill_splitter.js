46843471ba85cc6525feb690a7f3b6f20e1f6f87
Jennifer Loza-Marin <jenniferlozamarin@usf.edu>
2024-09-07 20:18:02 -0400

    Add output functionality to display bill, tip, and total

--- bill_splitter.js
+++ bill_splitter.js
@@ -2,3 +2,5 @@
 const bill = 175;
 const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
 
+// Task 2: Output details
+console.log(`The bill was $${bill}, the tip was $${tip}, and the total value is $${bill + tip}`); // Showcases the bill amount, the tip amount, and the total amount.
