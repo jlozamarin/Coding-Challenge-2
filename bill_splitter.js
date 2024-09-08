d3852bb7f2b4b71bea3c81a675f556c2ab08a104
Jennifer Loza-Marin <jenniferlozamarin@usf.edu>
2024-09-07 21:27:08 -0400

    Implement and populate arrays for bills, tips, and totals

--- bill_splitter.js
+++ bill_splitter.js
@@ -15,3 +15,27 @@ const testBill = 100;
 const testTip = calculateTip(testBill);
 
 console.log(`For a bill of $${testBill}, the tip is $${testTip}, and the total value is $${testBill + testTip}`); // Output: For a bill of $100, the tip is $15, and the total value is $115.
+
+// Task 4: Utilize arrays
+
+// Data set 1: Bill amounts of 275, 40, and 430
+const bills1 = [275, 40, 430];
+const tips1 = bills1.map(bill => calculateTip(bill));
+const totals1 = bills1.map((bill, index) => bill + tips1[index]);
+
+// Output:
+console.log("Data set 1:");
+console.log("Bills:", bills1);
+console.log("Tips:", tips1);
+console.log("Totals:", totals1);
+
+// Data set 2: Bill amounts of 125, 555, and 44
+const bills2 = [125,555,44];
+const tips2 = bills2.map(bill=> calculateTip(bill));
+const totals2 = bills2.map((bill,index) => bill + tips2[index]);
+
+//Output:
+console.log("Data set 2:");
+console.log("Bills:", bills2);
+console.log("Tips:", tips2);
+console.log("Totals:", totals2);
